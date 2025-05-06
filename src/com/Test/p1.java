64891d79581471010cccfebb3b1e2fc1818e20d2
Anusha T J <anushatj2004@gmail.com>
2025-05-06 09:51:46 +0530

    Update p1.java

--- src/com/Test/p1.java
+++ src/com/Test/p1.java
@@ -1,9 +1,14 @@
 package com.Test;
-
+im[ort java.util.Scanner;
 public class p1 {
 
 	public static void main(String[] args) {
 		System.out.println("heloo world");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		int sq=n*n;
+		System.out.println("square = "+sq);
 	}
 
 }
